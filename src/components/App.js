f6d43c20eba68c7e1b99fc5c68b00de70708a9cd
areenkh <areen.khalaila@gmail.com>
2024-08-19 23:18:02 -0400

    add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FollowersAnalysis from './FollowersAnalysis';
 
 function App() {
+    useEffect(() => {
+        const footer = document.querySelector('footer');
+        footer.style.width = window.innerWidth + 'px';
+    }, []);
+
     return (
-        <div className="app">
-            <header className="app-header">
-                <h1>BackTrack - Instagram Follow Check</h1>
-            </header>
-            <main className="main-content">
-                <FollowersAnalysis />
-                <Footer />
-            </main>
-        </div>
+            <div className="app">
+                <header className="app-header">
+                    <h1>BackTrack - Instagram Follow Check</h1>
+                </header>
+                <main className="main-content">
+                    <FollowersAnalysis />
+                </main>
+            </div>
     );
 }
 
